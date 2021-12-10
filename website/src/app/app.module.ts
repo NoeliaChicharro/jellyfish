14f53a99f388fa38de775c4140efd6d81fa2d988
devops <devops@marisma.net>
2021-12-10 11:21:57 +0100

    add main section

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
