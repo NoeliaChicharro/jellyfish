e6ce2c9a8ea586d06af8c8889a41a9e5da3c35fd
devops <devops@marisma.net>
2021-12-17 10:57:14 +0100

    add card component

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
+import {MatCard, MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    MainComponent
+    MainComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
